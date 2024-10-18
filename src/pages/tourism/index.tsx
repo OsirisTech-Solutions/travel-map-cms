56ac88dd86d34831bf86d1f14a6f020633bbea33
Thành Công <thanhcongns94@gmail.com>
2024-10-18 08:16:13 +0700

    feat: 🎸 add form

--- src/pages/tourism/index.tsx
+++ src/pages/tourism/index.tsx
@@ -1,136 +1,115 @@
-import { ProList } from '@ant-design/pro-components';
-import { Button, Space, Tag } from 'antd';
-import request from 'umi-request';
-
-type GithubIssueItem = {
-  url: string;
-  id: number;
-  number: number;
-  title: string;
-  labels: {
-    name: string;
-    color: string;
-  }[];
-  state: string;
-  comments: number;
-  created_at: string;
-  updated_at: string;
-  closed_at?: string;
-};
+import { PlusCircleOutlined } from '@ant-design/icons';
+import { Button, Flex, Form, Input, Modal, Table } from 'antd';
+import { useState } from 'react';
 
 const Tourism = () => {
+  const [open, setOpen] = useState(false);
+
+  const showModal = () => {
+    setOpen(true);
+  };
+  const handleOk = () => {
+    setOpen(false);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address: '10 Downing Street',
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address: '10 Downing Street',
+    },
+  ];
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+
   return (
-    <>
-      <ProList<GithubIssueItem>
-        toolBarRender={() => {
-          return [
-            <Button
-              key="3"
-              type="primary"
-            >
-              新建
-            </Button>,
-          ];
-        }}
-        search={{
-          filterType: 'light',
-        }}
-        rowKey="name"
-        headerTitle="基础列表"
-        request={async (params = {} as Record<string, any>) =>
-          request<{
-            data: GithubIssueItem[];
-          }>('https://proapi.azurewebsites.net/github/issues', {
-            params,
-          })
-        }
-        pagination={{
-          pageSize: 5,
-        }}
-        showActions="hover"
-        metas={{
-          title: {
-            dataIndex: 'user',
-            title: '用户',
-          },
-          avatar: {
-            dataIndex: 'avatar',
-            search: false,
-          },
-          description: {
-            dataIndex: 'title',
-            search: false,
-          },
-          subTitle: {
-            dataIndex: 'labels',
-            render: (_, row) => {
-              return (
-                <Space size={0}>
-                  {row.labels?.map((label: { name: string }) => (
-                    <Tag
-                      color="blue"
-                      key={label.name}
-                    >
-                      {label.name}
-                    </Tag>
-                  ))}
-                </Space>
-              );
-            },
-            search: false,
-          },
-          actions: {
-            render: (text, row) => [
-              <a
-                href={row.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                key="link"
-              >
-                链路
-              </a>,
-              <a
-                href={row.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                key="warning"
-              >
-                报警
-              </a>,
-              <a
-                href={row.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                key="view"
-              >
-                查看
-              </a>,
-            ],
-            search: false,
-          },
-          status: {
-            // 自己扩展的字段，主要用于筛选，不在列表中显示
-            title: '状态',
-            valueType: 'select',
-            valueEnum: {
-              all: { text: '全部', status: 'Default' },
-              open: {
-                text: '未解决',
-                status: 'Error',
-              },
-              closed: {
-                text: '已解决',
-                status: 'Success',
-              },
-              processing: {
-                text: '解决中',
-                status: 'Processing',
-              },
-            },
-          },
-        }}
+    <Flex vertical>
+      <Flex>
+        <Button
+          type="primary"
+          icon={<PlusCircleOutlined />}
+          onClick={() => setOpen(true)}
+        >
+          Thêm mới
+        </Button>
+      </Flex>
+
+      <Table
+        dataSource={dataSource}
+        columns={columns}
       />
-    </>
+
+      <Modal
+        title="Tạo loại hình mới"
+        centered
+        open={open}
+        onOk={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+        width={1000}
+      >
+        <Form
+          name="wrap"
+          labelCol={{ flex: '110px' }}
+          labelAlign="left"
+          labelWrap
+          wrapperCol={{ flex: 1 }}
+          colon={false}
+          style={{ maxWidth: 600 }}
+        >
+          <Form.Item
+            label="Tên loại hình"
+            name="title"
+            rules={[{ required: true, message: 'Nhập tên loại hình' }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Mô tả"
+            name="description"
+            rules={[{ required: true, message: 'Nhập mô tả về địa danh' }]}
+          >
+            <Input.TextArea />
+          </Form.Item>
+
+          <Form.Item label=" ">
+            <Button
+              type="primary"
+              htmlType="submit"
+            >
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </Flex>
   );
 };
 
